72b6653b4f18a5ea7615b9cf6d83ccac2b703c03
patryk <pszczepanski@webtechnika.pl>
2019-09-16 14:47:49 +0200

    And still fixes

--- getpaid/backends/payu_rest/__init__.py
+++ getpaid/backends/payu_rest/__init__.py
@@ -14,6 +14,7 @@ from django.core.exceptions import ImproperlyConfigured
 from django.urls import reverse
 from django.utils import six
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from six.moves.urllib.parse import urlencode
 
 from getpaid import signals
